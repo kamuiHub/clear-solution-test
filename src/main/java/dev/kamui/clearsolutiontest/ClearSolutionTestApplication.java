431ec6d1bde724f01d0e9ebea3848302d4af97f3
kamui <evgennkuvs@gmail.com>
2024-04-28 15:14:42 +0300

    first commit

---
+++ src/main/java/dev/kamui/clearsolutiontest/ClearSolutionTestApplication.java
@@ -0,0 +1,13 @@
+package dev.kamui.clearsolutiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearSolutionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kamui/clearsolutiontest/ClearSolutionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kamui.clearsolutiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
